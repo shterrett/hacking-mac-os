15356e6f836c2d30045e37b1f882bcd0b3e43006
Stuart Terrett <shterrett@gmail.com>
2017-01-11 21:48:19 -0500

    Project 17

---
+++ Project17/Project17/Ball.swift
@@ -0,0 +1,15 @@
+//
+//  Ball.swift
+//  Project17
+//
+//  Created by Stuart Terrett on 1/10/17.
+//  Copyright © 2017 Stuart Terrett. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+
+class Ball: SKSpriteNode {
+    var row = -1
+    var col = -1
+}
